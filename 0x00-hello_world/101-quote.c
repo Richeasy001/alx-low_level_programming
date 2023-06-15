43d0030e1f9237372329d50aa4eaf8155de4d319
richeasy001 <tenebe.osikhena@gmail.com>
2023-06-15 14:06:31 -0700

    print without printf and put

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that will print a line in standard error
+ * Return: 1 (Successfull)
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
