6b77363388463f2bb12f8ad69be43b6eab49676d
richeasy001 <tenebe.osikhena@gmail.com>
2023-06-15 12:06:54 -0700

    betty with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This program will give an output using printf function
+ * Return: 0 (Completed)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
