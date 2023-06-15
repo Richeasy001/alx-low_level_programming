14e71124ac35a85bd38b5e318e157b6522b65b7e
richeasy001 <tenebe.osikhena@gmail.com>
2023-06-15 11:52:11 -0700

    C puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This program will print a line with puts function
+ * Return: 0 (Complete)
+ */
+int main(void)
+{
+puts("\"rogramming is like building a multilingual puzzle");
+return (0);
+}
