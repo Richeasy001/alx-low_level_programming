1498e30f0cbab38804e87ca66403c7f3f78d59fc
richeasy001 <tenebe.osikhena@gmail.com>
2023-06-16 21:45:27 -0700

    print 1 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print 1 to 9
+ * Return: 0 (completed)
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	print("\n")
+	return (0);
+}
