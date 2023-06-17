495da143c4e5ca6720af1aa2b57877c1c7bb8e61
richeasy001 <tenebe.osikhena@gmail.com>
2023-06-16 23:34:14 -0700

    print 1 to 9 wit space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print base 10 with comma and space
+ * Return: 0 (completed)
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 48; m <= 57; m++)
+	{
+		putchar(m);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
