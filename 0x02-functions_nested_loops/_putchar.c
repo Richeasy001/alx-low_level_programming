acb544783450a450d4b816aba2035677b5f567e3
richeasy001 <tenebe.osikhena@gmail.com>
2023-06-21 11:05:15 -0700

    Putchar script

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * This is expected to put out charaters
+ * Return: 0 successful
+ * If error, return -1 and appropriately set errno
+ */
+int _putchar(char c)
+{
+	return (write(1, 8c, 1));
+}
