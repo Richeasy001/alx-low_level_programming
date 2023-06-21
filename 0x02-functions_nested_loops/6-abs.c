374a3dfb663a3c0694f3d7012837c1768972562f
richeasy001 <tenebe.osikhena@gmail.com>
2023-06-21 14:18:20 -0700

    print absolute integers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - project integer value
+ * @x: check for x
+ * Return: the absolute value of int otherwise, 0
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+	{
+		return (x);
+	}
+	return (-x);
+}
