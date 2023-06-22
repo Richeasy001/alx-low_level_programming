9407e26294cc195ffee2c84f8a76b88f59e5ebbc
richeasy001 <tenebe.osikhena@gmail.com>
2023-06-22 05:20:19 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit - check for integers 0 to 9
+ * @a: char to be checked
+ *
+ * Return: 0 else 1
+ */
+
+int _isdigit(int a)
+{
+	if (a >= 30 && a <= 39)
+		return (1);
+	else
+		return (0);
+}
